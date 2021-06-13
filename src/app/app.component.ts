6b0e17f179f2f6066adee3796fe137b655e2ba53
cromero82 <soportecarlosromero@gmail.com>
2021-06-12 20:20:33 -0500

    agregan los entornos de prod y desa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { environment } from 'environments/environment';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,4 +6,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+
+ constructor(){
+  console.log('Produccion = ' + environment.production);
+ }
+}
